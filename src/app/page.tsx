67b1c7616268cc3b9206bc10aeba76e2ab939b45
Itserge1 <realitserge@gmail.com>
2026-02-15 13:40:13 -0500

    update change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
     return (
         <div>
             <main>
-                <p>Test changes</p>
                 <HomePage />
             </main>
         </div>
