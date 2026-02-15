cea5b7fb500478070f2aa783461d7fda7db71a74
Itserge1 <realitserge@gmail.com>
2026-02-15 13:32:52 -0500

    remove myclient

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import HomePage from "@/components/Homepage/Homepage";
+
+export default function Home() {
+    return (
+        <div>
+            <main>
+                <HomePage />
+            </main>
+        </div>
+    );
+}
